e9440c91fd7480936c49a71bf60d4422ca33c8df
Tomas Smetana <tomas@smetana.name>
2022-03-08 19:26:35 +0100

    Update for newer autotools

--- autogen.sh
+++ autogen.sh
@@ -5,16 +5,16 @@
 # but aren't being created automtically
 touch NEWS README AUTHORS ChangeLog;
 
-echo "Running aclocal..."
-aclocal
-echo "Running autoheader..."
-autoheader
-echo "Running automake..."
-automake -ac
-echo "Running autoconf..."
-autoconf
-echo "Running configure..."
-./configure;
-echo;
-echo "Now type 'make' to build the project.";
+echo "Running aclocal..." && \
+aclocal && \
+echo "Running autoheader..." && \
+autoheader && \
+echo "Running automake..." && \
+automake -ac && \
+echo "Running autoconf..." && \
+autoconf && \
+echo "Running configure..." && \
+./configure && \
+echo && \
+echo "Now type 'make' to build the project." && \
 echo;
